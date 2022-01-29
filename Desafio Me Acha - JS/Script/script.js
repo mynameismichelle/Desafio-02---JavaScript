731b2d262cb7c848bbb655801ecc250aa4b27fd1
mynameismichelle <90280444+mynameismichelle@users.noreply.github.com>
2022-01-29 20:04:27 -0300

    Add files via upload

---
+++ Desafio Me Acha - JS/Script/script.js
@@ -0,0 +1,26 @@
+window.onload = function(){
+    function resultadoCep(dadosCep){
+        for(let campo in dadosCep){
+            if(document.querySelector(`#${campo}`)){
+                document.querySelector(`#${campo}`).value = dadosCep[campo]
+            }
+        }
+    }
+    let dadosCep = async function(cep){
+        let url = `https://viacep.com.br/ws/${cep}/json`;
+        try{
+            let dadosFetch = await fetch(url);
+            let dadosJson = await dadosFetch.json();
+            resultadoCep(dadosJson)
+        }catch(error){
+            alert(error);
+        }
+    }
+
+    const btnBuscar = document.querySelector("#buscarCep")
+    const CEP = document.querySelector("#numeroCep")
+    btnBuscar.addEventListener("click", function(){
+        dadosCep(CEP.value)
+    })
+
+}
